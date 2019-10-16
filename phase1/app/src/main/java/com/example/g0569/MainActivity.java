58a8845609719c92a1adaf6e0c03941204f13c38
Tongkun <kennardzhang@gmail.com>
2019-10-16 00:55:13 -0400

    Initialize the project with Empty Activity

---
+++ phase1/app/src/main/java/com/example/g0569/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.g0569;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
