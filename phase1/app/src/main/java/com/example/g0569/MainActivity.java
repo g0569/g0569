36feddde2bdecccba06699e6944f5116a67947a9
Tongkun Zhang <tongkun.zhang@mail.utoronto.ca>
2019-10-19 23:40:24 -0400

    Add handler in MainActivity to jump between different views
    Use util.Constants to store global constants.
    Upload the picture resources for previous demo

--- phase1/app/src/main/java/com/example/g0569/MainActivity.java
+++ phase1/app/src/main/java/com/example/g0569/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.g0569;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.g0569.utils.Constants;
+import com.example.g0569.view.MainMenuView;
 
+public class MainActivity extends Activity {
+
+    private MainMenuView mainMenuView;
+
+  @SuppressLint("HandlerLeak")
+  private Handler handler = new Handler() {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void handleMessage(Message msg) {
+      if (msg.what == Constants.TO_MENU_VIEW) {
+        toMenuView();
+      } else if (msg.what == Constants.TO_LOGIN_VIEW) {
+        toLoginView();
+      }
     }
+  };
+
+  private void toMenuView() {
+    if (mainMenuView == null) {
+      mainMenuView = new MainMenuView(this);
+    }
+    setContentView(mainMenuView);
+  }
+
+  private void toLoginView() {
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    this.getWindow()
+        .setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    MainMenuView mainMenuView = new MainMenuView(this);
+    setContentView(mainMenuView);
+  }
 }
---
+++ phase1/app/src/main/java/com/example/g0569/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.example.g0569.utils;
+
+public interface Constants {
+    int TO_MENU_VIEW = 0;
+    int TO_LOGIN_VIEW = 1;
+    int TO_END_VIEW = 2;
+    int END_GAME = 9;
+}
