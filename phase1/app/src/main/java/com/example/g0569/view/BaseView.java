28c41414cd4e2e584b70cd9060d14f8c7a0c3f57
Tongkun Zhang <tongkun.zhang@mail.utoronto.ca>
2019-10-19 22:05:20 -0400

    Create BaseView as a parent of other views
    Update AndroidManifest.xml to support landscape

---
+++ phase1/app/src/main/java/com/example/g0569/view/BaseView.java
@@ -0,0 +1,55 @@
+package com.example.g0569.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+import com.example.g0569.MainActivity;
+
+public class BaseView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+  protected float scalex;
+  protected float scaley;
+  protected float screen_width;
+  protected float screen_height;
+  protected boolean threadFlag;
+  protected Paint paint;
+  protected Canvas canvas;
+  protected SurfaceHolder sfh;
+  protected Thread thread;
+
+  protected MainActivity mainActivity;
+
+  public BaseView(Context context) {
+    super(context);
+    sfh = this.getHolder();
+    sfh.addCallback(this);
+    paint = new Paint();
+    mainActivity = (MainActivity) context;
+  }
+
+  @Override
+  public void surfaceCreated(SurfaceHolder holder) {
+    screen_width = this.getWidth();
+    screen_height = this.getHeight();
+    threadFlag = true;
+  }
+
+  @Override
+  public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+  @Override
+  public void surfaceDestroyed(SurfaceHolder holder) {
+    threadFlag = false;
+  }
+
+  public void draw() {}
+
+  @Override
+  public void run() {}
+
+  public void setThreadFlag(boolean threadFlag) {
+    this.threadFlag = threadFlag;
+  }
+}
