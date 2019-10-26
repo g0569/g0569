c7634d76e1e813d2294f53789d230852afe592f4
shuzo <shuzo.fukunaga@mail.utoronto.ca>
2019-10-26 11:31:01 -0400

    This is a star, one of the items.

---
+++ phase1/app/src/main/java/com/example/g0569/view/Level_3/Star.java
@@ -0,0 +1,17 @@
+package com.example.g0569.view.Level_3;
+
+import android.graphics.BitmapFactory;
+import android.content.res.Resources;
+import android.graphics.Rect;
+
+import com.example.g0569.R;
+
+public class Star extends ThrownItem {
+
+  public Star(float screen_width, float screen_height, Resources resource) {
+    super(screen_width, screen_height, resource);
+    appearance = BitmapFactory.decodeResource(resource, R.drawable.star);
+    damage = 20;
+    src_rect = new Rect(0, 0, appearance.getWidth(), appearance.getHeight());
+  }
+}
