ecff2ba7fc5b8315ad49ade8365f33d1c70c5e50
Tongkun Zhang <tongkun.zhang@mail.utoronto.ca>
2019-10-27 01:57:40 -0400

    MainActivity now can handle click events in LoginPage and SignUpPage

--- phase1/app/src/main/java/com/example/g0569/MainActivity.java
+++ phase1/app/src/main/java/com/example/g0569/MainActivity.java
@@ -7,54 +7,89 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 
+import com.example.g0569.module.game.GameManager;
 import com.example.g0569.utils.Constants;
 import com.example.g0569.view.BossView;
 import com.example.g0569.view.MainMenuView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     private MainMenuView mainMenuView;
+    private GameManager gameManager;
 
-  @SuppressLint("HandlerLeak")
-  private Handler handler = new Handler() {
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == Constants.TO_MENU_VIEW) {
+                toMenuView();
+            } else if (msg.what == Constants.TO_LOGIN_VIEW) {
+                toLoginView();
+            } else if (msg.what == Constants.TO_SIGNUP_VIEW) {
+                toSignUpView();
+            }
+        }
+    };
+
+    private void toSignUpView() {
+        mainMenuView = null;
+        setContentView(R.layout.page_signup);
+    }
+
+    private void toMenuView() {
+        if (mainMenuView == null) {
+            mainMenuView = new MainMenuView(this);
+        }
+        setContentView(mainMenuView);
+    }
+
+    private void toLoginView() {
+        mainMenuView = null;
+        setContentView(R.layout.page_login);
+    }
+
     @Override
-    public void handleMessage(Message msg) {
-      if (msg.what == Constants.TO_MENU_VIEW) {
-        toMenuView();
-      } else if (msg.what == Constants.TO_LOGIN_VIEW) {
-        toLoginView();
-      }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow()
+                .setFlags(
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        gameManager = new GameManager(this);
+
     }
-  };
 
-  private void toMenuView() {
-    if (mainMenuView == null) {
-      mainMenuView = new MainMenuView(this);
+
+    public Handler getHandler() {
+        return handler;
     }
-    setContentView(mainMenuView);
-  }
 
-  private void toLoginView() {
-    setContentView(R.layout.page_login);
-  }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case (R.id.login_button):
+                EditText email = (EditText) findViewById(R.id.email);
+                EditText password = (EditText) findViewById(R.id.passsword);
+                gameManager.login(email.getText().toString(), password.getText().toString());
+                break;
+            case (R.id.to_signup):
+                handler.sendEmptyMessage(Constants.TO_SIGNUP_VIEW);
+                break;
+            case (R.id.to_login):
+                handler.sendEmptyMessage(Constants.TO_LOGIN_VIEW);
+                break;
+            case (R.id.signup_button):
+                gameManager.signUp(((EditText) findViewById(R.id.signup_input_email)).getText().toString(),
+                        ((EditText) findViewById(R.id.signup_input_name)).getText().toString(),
+                        ((EditText) findViewById(R.id.passsword)).getText().toString());
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-    this.getWindow()
-        .setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//    MainMenuView mainMenuView = new MainMenuView(this);
-//    setContentView(mainMenuView);
-    BossView bossView = new BossView(this);
-    setContentView(bossView);
-  }
+        }
+    }
 
-  public Handler getHandler() {
-    return handler;
-  }
+
 }
