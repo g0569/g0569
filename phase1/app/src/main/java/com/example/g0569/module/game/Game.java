be026f2fb2c2e73b433c0c7e7843b5b6e9b7d33a
Tongkun Zhang <tongkun.zhang@mail.utoronto.ca>
2019-10-27 18:43:55 -0400

    Include GameManager in Game Super class

--- phase1/app/src/main/java/com/example/g0569/module/game/Game.java
+++ phase1/app/src/main/java/com/example/g0569/module/game/Game.java
@@ -2,6 +2,12 @@ package com.example.g0569.module.game;
 
 public abstract class Game {
 
+    private GameManager gameManager;
+
+    public Game(GameManager gameManager) {
+        this.gameManager = gameManager;
+    }
+
     public abstract void pause();
 
     public abstract void load();
