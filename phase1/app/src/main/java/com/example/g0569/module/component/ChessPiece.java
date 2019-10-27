e4234b88ea7ccc1a9f0125cddeabd320e2840528
Ruikai Fang <ruikai.fang@mail.utoronto.ca>
2019-10-26 21:00:59 -0400

    "Create some class for level 2 game."

---
+++ phase1/app/src/main/java/com/example/g0569/module/component/ChessPiece.java
@@ -0,0 +1,33 @@
+package com.example.g0569.module.component;
+
+import com.example.g0569.module.utils.Coordinate;
+
+
+public class ChessPiece extends NonPlayerItem{
+    //    should we implement classes for chess pieces?
+//    These classes to be the type of this instance variable?
+//    private Coordinate coordinate;
+    private String appearance;
+    private int power; // The amount for the chess piece can attack in one round.
+    private int health; // The amount of the hp for the chess piece.
+
+    public ChessPiece(int x, int y, int power, int health) {
+        super(new Coordinate(x,y));
+        this.power = power;
+        this.health = health;
+    }
+
+//    private void set_location(float x, float y){
+//        this.coordinate.setXY(x,y);
+//    } This method should be implemented in the LevelTwoPlayer.
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void action() {
+
+    }
+}
---
+++ phase1/app/src/main/java/com/example/g0569/module/component/StarChessPiece.java
@@ -0,0 +1,8 @@
+package com.example.g0569.module.component;
+
+
+/**
+ * A class for the chess piece represented by a star.
+ */
+public class StarChessPiece{
+}
