6cedf639af11cf0516afe61d9f68db322615140b
Tongkun Zhang <tongkun.zhang@mail.utoronto.ca>
2019-10-28 06:08:25 -0400

    Using MazeHelper to read a maze from 2d array in Phase 1

--- phase1/app/src/main/java/com/example/g0569/module/game/MazeGame.java
+++ phase1/app/src/main/java/com/example/g0569/module/game/MazeGame.java
@@ -1,6 +1,11 @@
 package com.example.g0569.module.game;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 import com.example.g0569.module.component.Item;
+import com.example.g0569.module.component.Maze.Components.MazePlayer;
+import com.example.g0569.module.component.Maze.MazeHelper;
 import com.example.g0569.module.utils.Coordinate;
 import com.example.g0569.utils.Constants;
 
@@ -9,44 +14,62 @@ import java.util.ArrayList;
 
 public class MazeGame extends Game {
 
-  private float grid_width;
+    private float grid_width;
+    private float grid_height;
 
-  private float grid_height;
+    private Coordinate startpoint;
 
-  private Coordinate startpoint;
-
-  public Item[][] MyMazeItem = new Item[Constants.GRID_NUM][Constants.GRID_NUM];
-
-  public MazeGame(GameManager gameManager) {
-      super(gameManager);
-      startpoint = new Coordinate(10, 10);
-      grid_width = gameManager.getScreen_width() / Constants.GRID_NUM;
-      grid_height = gameManager.getScreen_height() / Constants.GRID_NUM;
-  }
-
-  @Override
-  public void pause() {}
-
-  @Override
-  public void load() {}
-
-  public float getGrid_width() {
-    return grid_width;
-  }
-
-  public float getGrid_height() {
-    return grid_height;
-  }
+    private Item[][] myMazeItem = new Item[Constants.GRID_NUM][Constants.GRID_NUM];
+    private MazePlayer mazePlayer;
 
 
-  public Coordinate getStartpoint() {
-    return this.startpoint;
-  }
+    public MazeGame(GameManager gameManager) {
+        super(gameManager);
+        startpoint = new Coordinate(10, 10);
+        grid_width = gameManager.getScreen_width() / Constants.GRID_NUM;
+        grid_height = gameManager.getScreen_height() / Constants.GRID_NUM;
+        mazePlayer = new MazePlayer(this);
+    }
 
-  public void move(float x, float y){}
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void load() {
+    }
+
+    public float getGrid_width() {
+        return grid_width;
+    }
+
+    public float getGrid_height() {
+        return grid_height;
+    }
+
+
+    public Coordinate getStartpoint() {
+        return this.startpoint;
+    }
+
+    public void move(float x, float y) {
+    }
 
 //  public void createItem(float a, float b) {}
 
-  public void setMyMazeItem() {
-  }
+    public void setMyMazeItem() {
+        MazeHelper.loadMaze(myMazeItem, this);
+    }
+
+    public void draw(Canvas canvas, Paint paint) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                myMazeItem[i][j].draw(canvas, paint);
+            }
+        }
+    }
+
+    public Item[][] getMyMazeItem() {
+        return myMazeItem;
+    }
 }
