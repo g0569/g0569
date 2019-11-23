4eb085b78e0a3395269e2285f2530c5fe91259af
shuzo <shuzo.fukunaga@mail.utoronto.ca>
2019-11-23 15:01:59 -0500

    made the structure better

--- phase2/app/src/main/java/com/example/g0569/module/component/Boss/ShootButton.java
+++ phase2/app/src/main/java/com/example/g0569/module/component/Boss/ShootButton.java
@@ -1,6 +1,7 @@
 package com.example.g0569.module.component.Boss;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 
 import com.example.g0569.module.game.Game;
@@ -8,29 +9,31 @@ import com.example.g0569.module.utils.Coordinate;
 
 public class ShootButton extends Button {
 
-    public ShootButton(Game game, float screenWidth, float screenHeight) {
-        super(game, screenWidth, screenHeight);
-        button_r = screenWidth / 16;
+  public float button_r;
 
-        // Sets coordinates of the button
-        x = screenWidth * 5 / 6;
-        y = screenHeight - button_r * 3 / 2;
-        coordinate = new Coordinate(x, y);
-    }
+  public ShootButton(Game game, float screenWidth, float screenHeight) {
+    super(game, screenWidth, screenHeight);
+    button_r = screenWidth / 16;
 
-    @Override
-    public void draw(Canvas canvas, Paint paint) {
-        paint.setStyle(Paint.Style.FILL);
-        if (colorChanged % 3 == 0){
-            paint.setColor(red);
-        }else if (colorChanged % 3 == 1){
-            paint.setColor(yellow);
-        }
-        else {
-            paint.setColor(blue);
-        }
+    // Sets coordinates of the button
+    x = screenWidth * 5 / 6;
+    y = screenHeight - button_r * 3 / 2;
+    coordinate = new Coordinate(x, y);
+  }
 
-//    paint.setColor(Color.RED);
-        canvas.drawCircle(coordinate.getX(), coordinate.getY(), button_r, paint);
-    }
+  @Override
+  public void draw(Canvas canvas, Paint paint) {
+    paint.setStyle(Paint.Style.FILL);
+    paint.setColor(Color.RED);
+    canvas.drawCircle(coordinate.getX(), coordinate.getY(), button_r, paint);
+  }
+
+  /**
+   * Returns the radius of the circle
+   *
+   * @return the radius
+   */
+  public float getR() {
+    return button_r;
+  }
 }
