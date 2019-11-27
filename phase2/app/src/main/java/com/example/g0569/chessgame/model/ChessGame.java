c15c72e46773407f15aef8073680c144d8a9b304
EuroCV <wangluojia981122@gmail.com>
2019-11-26 21:11:10 -0500

    Implemented the DataDecoding method of chess piece sql data.

--- phase2/app/src/main/java/com/example/g0569/chessgame/model/ChessGame.java
+++ phase2/app/src/main/java/com/example/g0569/chessgame/model/ChessGame.java
@@ -15,6 +15,7 @@ public class ChessGame extends BaseGame {
   private int winNumbers;
   private LevelTwoPlayer l2player;
   private int clickNumbers = 1;
+  private String difficulty;
   private ChessPieceFactory chessPieceFactory;
   private ChessSQLiteAccessInterface boardData;
   private List<ChessPiece> NPCData = new ArrayList<>();
@@ -60,11 +61,42 @@ public class ChessGame extends BaseGame {
   }
 
   public void decodeNPCData() {
-    String s = boardData.getChessBoardData();
-    //    float x = 0;
-    //    float y = 0;
-    //    String type = "";
-    //    placeNPCChess(x,y,type);
+    String chessString = boardData.getChessBoardData();//get data before the start of each round.
+    String[] chessDataList = chessString.split(".");//suppose we are getting string like"EZ.star,1,1.circle,1,2.HD.circle,1,1.nochess,1,2"
+    int hardNPCIndex = 0;
+    boolean hard_found = false;
+    while (hardNPCIndex <= chessDataList.length & !hard_found) {
+      if (chessDataList[hardNPCIndex] == "HD") {
+        hard_found = true;//find the starting index of the NPC(Hard difficulty)
+      }
+      if (!hard_found) {
+        hardNPCIndex++;
+      }
+    }
+    //place chess pieces in easy mode.
+    if(difficulty == "easy"){
+      int count = 1;// the first element in the list is obviously "EZ"
+      while(count < hardNPCIndex){
+        String[] curr_chess = chessDataList[count].split(",");// for example:["star","1","1"]
+        String type = curr_chess[0];
+        float x = Float.parseFloat(curr_chess[1]);
+        float y = Float.parseFloat(curr_chess[2]);
+        placeNPCChess(x,y,type);
+        count ++;
+      }
+    }
+    //place chess pieces in hard mode.
+    if(difficulty == "hard"){
+      int count = hardNPCIndex + 1;// start from the next element of element "HD"
+      while(count < chessDataList.length){
+        String[] curr_chess = chessDataList[count].split(",");
+        String type = curr_chess[0];
+        float x = Float.parseFloat(curr_chess[1]);
+        float y = Float.parseFloat(curr_chess[2]);
+        placeNPCChess(x,y,type);
+        count ++;
+      }
+    }
     // TODO when decoding the string, for each chess piece, call placeNPCChess(x, y, type).
   }
 
