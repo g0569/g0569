fbf49f3e4e32b57e673df4b73230f8a27d907269
Tongkun Zhang <tongkun.zhang@mail.utoronto.ca>
2019-11-29 21:27:27 -0500

    Document and format the main activity

--- phase2/app/src/main/java/com/example/g0569/main/MainContract.java
+++ phase2/app/src/main/java/com/example/g0569/main/MainContract.java
@@ -3,8 +3,11 @@ package com.example.g0569.main;
 import com.example.g0569.base.BasePresenter;
 import com.example.g0569.base.BaseView;
 
+/** The Main contract interface. */
 public interface MainContract {
+  /** The interface View. */
   interface View extends BaseView<Presenter> {}
 
+  /** The interface Presenter. */
   interface Presenter extends BasePresenter {}
 }
