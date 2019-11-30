60a3805ccc3d7d13a74355061da01a03d2c8ef99
Ruikai Fang <ruikai.fang@mail.utoronto.ca>
2019-11-30 01:21:46 -0500

    Move typeLookUpMap and NPCs Bitmap decodeResource to GameView for all level to ues. Note: Still need to rescale the Bitmap and put it into the new "local" Bitmap to draw on the screen.

--- phase2/app/src/main/java/com/example/g0569/base/GameView.java
+++ phase2/app/src/main/java/com/example/g0569/base/GameView.java
@@ -2,74 +2,131 @@ package com.example.g0569.base;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public abstract class GameView extends SurfaceView implements SurfaceHolder.Callback, Runnable  {
+import com.example.g0569.R;
 
-    /** The Scale of the background. */
-    protected float scalex;
+import java.util.HashMap;
 
-    protected float scaley;
+public abstract class GameView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
 
-    /** The Screen size. */
-    protected float screenWidth;
+  /** The Scale of the background. */
+  protected float scalex;
 
-    protected float screenHeight;
+  protected float scaley;
 
-    /** The Thread flag. */
-    protected boolean threadFlag;
+  /** The Screen size. */
+  protected float screenWidth;
 
-    protected Paint paint;
-    protected Canvas canvas;
+  protected float screenHeight;
 
-    protected SurfaceHolder sfh;
-    protected Thread thread;
-    protected Activity activity;
+  /** The Thread flag. */
+  protected boolean threadFlag;
 
-    /**
-     * Instantiates a new Baseview.
-     *
-     * @param context the context
-     */
-    public GameView(Context context) {
-        super(context);
-        sfh = this.getHolder();
-        sfh.addCallback(this);
-        paint = new Paint();
-        activity = (Activity) context;
-    }
+  protected Paint paint;
+  protected Canvas canvas;
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        screenWidth = this.getWidth();
-        screenHeight = this.getHeight();
-        threadFlag = true;
-    }
+  protected SurfaceHolder sfh;
+  protected Thread thread;
+  protected Activity activity;
 
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+  private HashMap<String, Bitmap> typeLookUpMap;
 
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        threadFlag = false;
-    }
+  private Bitmap npc1;
+  private Bitmap npc2;
+  private Bitmap npc3;
+  private Bitmap npc4;
+  private Bitmap npc5;
+  private Bitmap npc6;
 
-    /** Draw this view. */
-    public void draw() {}
+  /**
+   * Instantiates a new Baseview.
+   *
+   * @param context the context
+   */
+  public GameView(Context context) {
+    super(context);
+    sfh = this.getHolder();
+    sfh.addCallback(this);
+    paint = new Paint();
+    activity = (Activity) context;
+    npc1 = BitmapFactory.decodeResource(getResources(), R.drawable.npc_l1);
 
-    @Override
-    public void run() {}
+    npc2 = BitmapFactory.decodeResource(getResources(), R.drawable.npc_l2);
 
-    /**
-     * Sets thread flag.
-     *
-     * @param threadFlag the thread flag
-     */
-    public void setThreadFlag(boolean threadFlag) {
-        this.threadFlag = threadFlag;
-    }
+    npc3 = BitmapFactory.decodeResource(getResources(), R.drawable.npc_l3);
 
+    npc4 = BitmapFactory.decodeResource(getResources(), R.drawable.npc_l4);
+
+    npc5 = BitmapFactory.decodeResource(getResources(), R.drawable.npc_l5);
+
+    npc6 = BitmapFactory.decodeResource(getResources(), R.drawable.npc_l6);
+  }
+
+  public HashMap<String, Bitmap> getTypeLookUpMap() {
+    return typeLookUpMap;
+  }
+
+  public void setTypeLookUpMap(HashMap<String, Bitmap> typeLookUpMap) {
+    this.typeLookUpMap = typeLookUpMap;
+  }
+
+  protected Bitmap getNpc1() {
+    return npc1;
+  }
+
+  protected Bitmap getNpc2() {
+    return npc2;
+  }
+
+  protected Bitmap getNpc3() {
+    return npc3;
+  }
+
+  protected Bitmap getNpc4() {
+    return npc4;
+  }
+
+  protected Bitmap getNpc5() {
+    return npc5;
+  }
+
+  protected Bitmap getNpc6() {
+    return npc6;
+  }
+
+  @Override
+  public void surfaceCreated(SurfaceHolder holder) {
+    screenWidth = this.getWidth();
+    screenHeight = this.getHeight();
+    threadFlag = true;
+  }
+
+  @Override
+  public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+  @Override
+  public void surfaceDestroyed(SurfaceHolder holder) {
+    threadFlag = false;
+  }
+
+  /** Draw this view. */
+  public void draw() {}
+
+  @Override
+  public void run() {}
+
+  /**
+   * Sets thread flag.
+   *
+   * @param threadFlag the thread flag
+   */
+  public void setThreadFlag(boolean threadFlag) {
+    this.threadFlag = threadFlag;
+  }
 }
